dda982f6b976389e26e4e7e630cf757ea0818ee9
bill-msi-laptop <bill-msi-laptop>
2021-12-25 22:35:18 +0800

    [master] Renamed a variable.

--- MockDatabase.go
+++ MockDatabase.go
@@ -8,22 +8,22 @@ type MockDatabase struct {
   Data []interface{}
 }
 
-func (dbPtr *MockDatabase) Query(query string, args ...interface{}) (IRows, error) {
+func (databasePtr *MockDatabase) Query(query string, args ...interface{}) (IRows, error) {
   rows := MockRows{
-    Data: dbPtr.Data,
+    Data: databasePtr.Data,
     First: true,
   }
   return &rows, nil
 }
 
-func (dbPtr *MockDatabase) QueryRow(query string, args ...interface{}) (IRow) {
+func (databasePtr *MockDatabase) QueryRow(query string, args ...interface{}) (IRow) {
   row := MockRow{
-    Data: dbPtr.Data,
+    Data: databasePtr.Data,
   }
   return &row
 }
 
-func (dbPtr *MockDatabase) Exec(query string, args ...interface{}) (sql.Result, error) {
-  dbPtr.Data = args
+func (databasePtr *MockDatabase) Exec(query string, args ...interface{}) (sql.Result, error) {
+  databasePtr.Data = args
   return nil, nil
 }
