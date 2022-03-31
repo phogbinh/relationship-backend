029cc09771dfe9f389c005c2517eb70d45875121
bill-msi-laptop <bill-msi-laptop>
2022-04-01 00:35:37 +0800

    [master] Corrected person id datatype.

--- Person.go
+++ Person.go
@@ -5,7 +5,7 @@ import(
 )
 
 type Person struct {
-  Id int
+  Id int64
   Nickname string `json:"nickname"`
   FirstName sql.NullString `json:"firstName"`
   MiddleName sql.NullString `json:"middleName"`
